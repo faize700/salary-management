1cd6c2e93adbb5a5b07468df842ba86f9415a17c
Faize <faize@gmail.com>
2026-08-20 00:03:57 +0530

    feat: scaffold Spring Boot backend

---
+++ src/test/java/com/acme/salary_management/SalaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.salary_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
